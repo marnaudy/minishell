db7a44d68a8e3a4e11c4e6311d21f3e853e6534e
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2022-05-04 12:08:51 +0200

    Add ft_strcat to libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: marnaudy <marnaudy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 11:33:18 by marnaudy          #+#    #+#             */
-/*   Updated: 2022/02/13 09:57:50 by marnaudy         ###   ########.fr       */
+/*   Updated: 2022/05/03 18:08:42 by marnaudy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strcat(char *s1, char *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
